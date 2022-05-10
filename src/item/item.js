1c26df8d6aaac4d12084b23159cd6533c75d5bbe
Tomas Sanchez <tms90@outlook.es>
2022-05-10 17:18:28 +0200

    Added item.js file with an array of items

---
+++ src/item/item.js
@@ -0,0 +1,24 @@
+const listOfItems = [
+  {
+    id: 1,
+    name: "Leche",
+    precio: 1,
+  },
+  {
+    id: 2,
+    name: "papas",
+    precio: 5,
+  },
+  {
+    id: 3,
+    name: "tomates",
+    precio: 2,
+  },
+  {
+    id: 4,
+    name: "pepinos",
+    precio: 1,
+  },
+];
+
+export default listOfItems;
